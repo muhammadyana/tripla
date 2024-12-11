2f068c0006b9617a3420a7fde93879676f4b0cc5
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-11 22:41:45 +0900

    Add validations to Follow model to prevent self-following

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -20,8 +20,17 @@
 #  follower_id  (follower_id => users.id)
 #
 class Follow < ApplicationRecord
+  # Relationships
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
 
+  # Validations
   validates :follower_id, uniqueness: { scope: :followed_id }
+  validate :cannot_follow_self
+
+  private
+
+  def cannot_follow_self
+    errors.add(:base, "Cannot follow yourself") if follower_id == followed_id
+  end
 end
