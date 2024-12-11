424a8654fb2d2b034b0718ce5aea39480d2bb30f
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-11 23:40:57 +0900

    Add unit test for follow model

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -22,5 +22,38 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:follower).class_name('User') }
+    it { should belong_to(:followed).class_name('User') }
+  end
+
+  describe 'validations' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    subject { create(:follow, follower: user1, followed: user2) }
+
+    it { should validate_uniqueness_of(:follower_id).scoped_to(:followed_id) }
+  end
+
+  describe 'custom validations' do
+    let(:user) { create(:user) }
+
+    context 'when attempting to follow self' do
+      subject { build(:follow, follower: user, followed: user) }
+
+      it 'is invalid' do
+        expect(subject).not_to be_valid
+        expect(subject.errors[:base]).to include("Cannot follow yourself")
+      end
+    end
+
+    context 'when following another user' do
+      let(:other_user) { create(:user) }
+      subject { build(:follow, follower: user, followed: other_user) }
+
+      it 'is valid' do
+        expect(subject).to be_valid
+      end
+    end
+  end
 end
