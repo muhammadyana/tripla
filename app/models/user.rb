5cedf85e3ee8e485e4d7ac284e568af460b63b1e
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-11 22:34:47 +0900

    Add relationships for sleep records and follow in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  # Relationships
+  has_many :sleep_records, dependent: :destroy
+  has_many :following_users, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :followings, through: :following_users, source: :followed
+  has_many :followed_users, foreign_key: :followed_id, class_name: 'Follow'
+  has_many :followers, through: :followed_users
 end
