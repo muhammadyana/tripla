a53eccceca734e45815d8e0af4821e931d682de2
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-11 22:56:09 +0900

    Implement follow, unfollow, and following? methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,19 @@ class User < ApplicationRecord
   has_many :followings, through: :following_users, source: :followed
   has_many :followed_users, foreign_key: :followed_id, class_name: 'Follow'
   has_many :followers, through: :followed_users
+
+
+  def follow(user)
+    return false if user == self
+
+    followings << user unless followings.include?(user)
+  end
+
+  def unfollow(user)
+    followings.delete(user)
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 end
