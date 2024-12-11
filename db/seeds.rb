944ea7860df46e3f1f97b36df6b8825242d31206
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-11 22:46:33 +0900

    Seed database with 10 fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  User.create!(
+    name: Faker::Name.name,
+    created_at: Time.now,
+    updated_at: Time.now
+  )
+end
