800aa6aa9d68cc0f547dcf19fc65979d3fc6fe95
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-12 23:41:05 +0900

    Refactor SleepRecordsController to use current_user for authentication and remove unnecessary user_id validation

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -1,42 +1,30 @@
 module Api
   module V1
     class SleepRecordsController < ApplicationController
-      before_action :validate_user_id
-      before_action :set_user
 
       def index
-        sleep_records = @user.sleep_records.desc
+        sleep_records = current_user.sleep_records.desc
         responder(:ok, SleepRecordSerializer.new(sleep_records))
       end
 
       def clock_in
-        @user.sleep_records.where(clock_out_time: nil).update_all(clock_out_time: Time.current)
+        current_user.sleep_records.where(clock_out_time: nil).update_all(clock_out_time: Time.current)
 
-        @user.sleep_records.create!(clock_in_time: Time.current)
+        current_user.sleep_records.create!(clock_in_time: Time.current)
 
-        sleep_records = @user.sleep_records.desc
+        sleep_records = current_user.sleep_records.desc
         responder(:created, SleepRecordSerializer.new(sleep_records))
       end
 
       def following_sleep_records
         sleep_records = SleepRecord
           .joins(:user)
-          .merge(User.joins(:followers).where(followers: { id: @user.id }))
+          .merge(User.joins(:followers).where(followers: { id: current_user.id }))
           .for_week
           .completed
           .order(duration_seconds: :desc)
 
         responder(:ok, SleepRecordSerializer.new(sleep_records))
-      end
-
-      private
-
-      def validate_user_id
-        params.require(:user_id)
-      end
-
-      def set_user
-        @user = User.find(params[:user_id])
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::API
   include ApiHelper
+
+  before_action :authenticate_user!
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_user!
+    user_id = params[:user_id] || params[:id]
+    params.require(:user_id)
+
+    @current_user = User.find_by(id: user_id)
+    unauthorized_error unless @current_user
+  end
+
+  def unauthorized_error
+    responder(:unauthorized, 'Authentication failed. Please provide a valid user ID.')
+  end
 end
