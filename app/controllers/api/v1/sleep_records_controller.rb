b4f17329e5c28c0204db0af147fc194736d1734a
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2024-12-13 00:19:47 +0900

    Implement caching for sleep records in SleepRecordsController to improve performance

--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -3,7 +3,9 @@ module Api
     class SleepRecordsController < ApplicationController
 
       def index
-        sleep_records = current_user.sleep_records.desc
+        sleep_records = Rails.cache.fetch("user_sleep_records_#{current_user.id}", expires_in: 1.hour) do
+          current_user.sleep_records.desc
+        end
         responder(:ok, SleepRecordSerializer.new(sleep_records))
       end
 
@@ -17,12 +19,14 @@ module Api
       end
 
       def following_sleep_records
-        sleep_records = SleepRecord
-          .joins(:user)
-          .merge(User.joins(:followers).where(followers: { id: current_user.id }))
-          .for_week
-          .completed
-          .order(duration_seconds: :desc)
+        sleep_records = Rails.cache.fetch("following_sleep_records_#{current_user.id}", expires_in: 1.hour) do
+          SleepRecord
+            .joins(:user)
+            .merge(User.joins(:followers).where(followers: { id: current_user.id }))
+            .for_week
+            .completed
+            .order(duration_seconds: :desc)
+        end
 
         responder(:ok, SleepRecordSerializer.new(sleep_records))
       end
