c95dcfe87674d5245abd24600f72544fc6e820bf
muhammadyana <muhammadyanaa@gmail.com>
2024-12-19 13:15:36 +0700

    Refactor Follow model for consistent string quoting style

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -21,8 +21,8 @@
 #
 class Follow < ApplicationRecord
   # Relationships
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followed, class_name: 'User'
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
 
   # Validations
   validates :follower_id, uniqueness: { scope: :followed_id }
