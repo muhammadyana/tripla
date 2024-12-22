9b6c083949889925c80ec978568963cd8fd9b94d
muhammadyana <muhammadyanaa@gmail.com>
2024-12-22 23:39:18 +0700

    Prevent duplicate index creation in follows migration

--- db/migrate/20241219061247_add_index_to_follows.rb
+++ db/migrate/20241219061247_add_index_to_follows.rb
@@ -1,6 +1,6 @@
 class AddIndexToFollows < ActiveRecord::Migration[7.2]
   def change
-    add_index :follows, :follower_id
-    add_index :follows, :followed_id
+    add_index :follows, :follower_id unless index_exists?(:follows, :follower_id)
+    add_index :follows, :followed_id unless index_exists?(:follows, :followed_id)
   end
 end
